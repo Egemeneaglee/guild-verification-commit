072513b69785747d80c8488f4a2d47e3dbe82592
Egemeneaglee <98287779+Egemeneaglee@users.noreply.github.com>
2026-03-05 22:54:40 +0300

    Add multi-language support

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T19:54:26.325Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-05T19:54:40.169Z
+Commit: Add multi-language support
