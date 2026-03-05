ac18d2d5dad07ccda4c55f8330464c56507648fa
Egemeneaglee <98287779+Egemeneaglee@users.noreply.github.com>
2026-03-05 22:55:58 +0300

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T19:55:50.519Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-05T19:55:58.333Z
+Commit: Resolve timezone issues in date handling
