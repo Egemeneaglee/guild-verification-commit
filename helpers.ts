edc3773f51f741add18b0fde722c445830b0616d
Egemeneaglee <98287779+Egemeneaglee@users.noreply.github.com>
2026-03-05 22:56:00 +0300

    Implement user profile customization

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T19:55:58.333Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-05T19:55:59.779Z
+Commit: Implement user profile customization
