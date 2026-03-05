a39fba04838db46db538cbecbcd0a89b4dd8d004
Egemeneaglee <98287779+Egemeneaglee@users.noreply.github.com>
2026-03-05 22:56:48 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T19:56:44.873Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-05T19:56:48.219Z
+Commit: Fix broken links in documentation
