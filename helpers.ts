737ec1f6488bb35516fe414dcaed87bc0b61af98
Egemeneaglee <98287779+Egemeneaglee@users.noreply.github.com>
2026-03-05 22:54:26 +0300

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T19:54:24.657Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-05T19:54:26.325Z
+Commit: Resolve timezone issues in date handling
